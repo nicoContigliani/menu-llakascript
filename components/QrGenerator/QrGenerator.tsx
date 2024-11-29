381b472044972b5205fdcc839f80dcac52530e0f
nicoContigliani <nico.contigliani>
2024-11-28 21:40:26 -0300

    shares

--- components/QrGenerator/QrGenerator.tsx
+++ components/QrGenerator/QrGenerator.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useQRCode } from 'next-qrcode'
+import ShareButtons from '../SocialShare/ShareButton'
 
 const QrGenerator = ({ dataqrs, nameCompanines }) => {
     const { Image } = useQRCode()
@@ -7,21 +8,26 @@ const QrGenerator = ({ dataqrs, nameCompanines }) => {
     return (
         <div>
             {dataqrs ? (
-                <Image
-                    text={dataqrs as string} 
-                    options={{
-                        type: 'image/jpeg',
-                        quality: 0.3,
-                        errorCorrectionLevel: 'M',
-                        margin: 3,
-                        scale: 4,
-                        width: 200,
-                        color: {
-                            dark: '#010599FF',
-                            light: '#FFBF60FF',
-                        },
-                    }}
-                />
+                <div>
+
+                    <Image
+                        text={dataqrs as string}
+                        options={{
+                            type: 'image/jpeg',
+                            quality: 0.3,
+                            errorCorrectionLevel: 'M',
+                            margin: 3,
+                            scale: 4,
+                            width: 200,
+                            color: {
+                                dark: '#010599FF',
+                                light: '#FFBF60FF',
+                            },
+                        }}
+                    />
+                    <ShareButtons url={dataqrs} title={'QR de ' + nameCompanines}
+                    />
+                </div>
             ) : (
                 ""
             )}
---
+++ components/SocialShare/ShareButton.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from 'react-share';
+
+interface ShareButtonProps {
+  url: string;
+  title: string;
+}
+
+const ShareButtons: React.FC<ShareButtonProps> = ({ url, title }) => {
+  return (
+    <div>
+      <FacebookShareButton url={url} title={title}>
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+
+      <TwitterShareButton url={url} title={title}>
+        <TwitterIcon size={32} round />
+      </TwitterShareButton>
+
+      <WhatsappShareButton url={url} title={title}>
+        <WhatsappIcon size={32} round />
+      </WhatsappShareButton>
+    </div>
+  );
+};
+
+export default ShareButtons;
